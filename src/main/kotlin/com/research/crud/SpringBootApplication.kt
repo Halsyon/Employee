55f5bc04c1cb2b7306a622f05357624c9feebb56
GrandEmetak <JekHamond@tutanota.com>
2023-02-14 20:47:10 +0500

    feat: Initial commit
    created Employee project

---
+++ src/main/kotlin/com/research/crud/SpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.research.crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlincrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlincrudApplication>(*args)
+}
