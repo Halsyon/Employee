8ac1ef0b679693fa1661741510526672d758f879
GrandEmetak <JekHamond@tutanota.com>
2023-02-14 22:05:56 +0500

    fix: updated Entity info

--- src/main/kotlin/com/research/crud/model/Employee.kt
+++ src/main/kotlin/com/research/crud/model/Employee.kt
@@ -3,8 +3,7 @@ package com.research.crud.model
 import lombok.AllArgsConstructor
 import lombok.Getter
 import lombok.Setter
-import javax.persistence.Entity
-import javax.persistence.Id
+import javax.persistence.*
 
 @Setter
 @Getter
@@ -13,6 +12,8 @@ import javax.persistence.Id
 data class Employee(
 
         @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        @Column(name = "id")
         var id: Long = 0,
         var name: String = "",
         var age: Long = 0,
