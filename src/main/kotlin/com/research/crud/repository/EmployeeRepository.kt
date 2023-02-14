c449ed4103430681a7f17237f2c655be0f5d9270
GrandEmetak <JekHamond@tutanota.com>
2023-02-14 21:44:48 +0500

    fit: updated comment info

--- src/main/kotlin/com/research/crud/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/research/crud/repository/EmployeeRepository.kt
@@ -3,5 +3,5 @@ package com.research.crud.repository
 import com.research.crud.model.Employee
 import org.springframework.data.repository.CrudRepository
 
-interface EmployeeRepository :CrudRepository<Employee, Long> {
+interface EmployeeRepository : CrudRepository<Employee, Long> {
 }
